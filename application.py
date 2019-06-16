b952cf6173102a30f6f3e288f34910dde3f4ce7d
suchitra <suchitra.nagarajan@mavs.uta.edu>
2019-06-16 17:31:50 -0600

    fivethierty

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import pyodbc
 import os
 application = app = Flask(__name__)
 
-#port = int(os.getenv("VCAP_APP_PORT", '5000'))
+port = int(os.getenv("VCAP_APP_PORT", '5000'))
 # port = os.getenv("VCAP_APP_PORT")
 
 
@@ -28,7 +28,7 @@ def home():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run('127.0.0.1',debug=True)
+    app.run('0.0.0.0',port=port)
 
     #app.run(host='0.0.0.0', port=port)
 
